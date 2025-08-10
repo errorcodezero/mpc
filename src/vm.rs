edbb6685ac14d16a89fd2bfa7f48c736be47f902
ErrorCode0 <me@errorcodezero.dev>
2025-08-10 12:55:49 -0400

    more instructions

--- src/vm.rs
+++ src/vm.rs
@@ -1,13 +1,29 @@
-const MEMORY: usize = 2 ^ 16;
+const MEMORY: usize = 0xFFFF;
 const HALT: u8 = 0x0;
 const LOAD_IMMEDIATE: u8 = 0x1;
-const LOAD_FROM_MEM: u8 = 0x2;
+// const LOAD_FROM_MEM: u8 = 0x2;
 const DUMP: u8 = 0x3;
+const ADD: u8 = 0x4;
+const SUB: u8 = 0x5;
+const MULT: u8 = 0x6;
+const DIV: u8 = 0x7;
+const AND: u8 = 0x8;
+const OR: u8 = 0x9;
+const NOT: u8 = 0xA;
+const XOR: u8 = 0xB;
+const LSHIFT: u8 = 0xC;
+const RSHIFT: u8 = 0xD;
+const PUSH: u8 = 0xE;
+const POP: u8 = 0xF;
+const DUMP_ACCUM: u8 = 0x10;
+const PUSH_ACCUM: u8 = 0x11;
 
 pub struct VM {
     mem: Vec<u8>,
     // like 6502 where last register is for setting flags
     regs: [u16; 16],
+    accum: u16,
+    sp: usize,
     instruc: usize,
     halted: bool,
 }
@@ -19,23 +35,91 @@ impl VM {
         if !self.halted {
             match opcode[0] {
                 LOAD_IMMEDIATE => {
-                    self.regs[opcode[1] as usize] =
-                        ((self.regs[2] as u16) << 8) | (self.regs[3] as u16);
+                    self.regs[opcode[1] as usize] = ((self.regs[2]) << 8) | (self.regs[3]);
                 }
-                LOAD_FROM_MEM => {
-                    let index = (((self.regs[2] as u16) << 8) | (self.regs[3] as u16)) as usize;
-                    self.regs[opcode[1] as usize] =
-                        ((self.mem[index] as u16) << 8) | (self.mem[index + 1] as u16);
-                }
+                // LOAD_FROM_MEM => {
+                //     let index = (((self.regs[2] as u16) << 8) | (self.regs[3] as u16));
+                //     self.regs[opcode[1] as usize] =
+                //         ((self.mem[index] as u16) << 8) | (self.mem[index + 1] as u16);
+                // }
                 DUMP => {
                     let reg = self.regs[opcode[1] as usize];
                     let index = (((opcode[2] as u16) << 8) | (opcode[3] as u16)) as usize;
                     self.mem[index] = (reg >> 8) as u8;
                     self.mem[index + 1] = reg as u8;
                 }
-                HALT | _ => {
+                ADD => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 + reg2;
+                }
+                SUB => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 - reg2;
+                }
+                MULT => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 * reg2;
+                }
+                DIV => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 / reg2;
+                }
+                AND => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 & reg2;
+                }
+                OR => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 | reg2;
+                }
+                NOT => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    self.accum = !reg1;
+                }
+                XOR => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    let reg2 = self.regs[opcode[2] as usize];
+                    self.accum = reg1 ^ reg2;
+                }
+                LSHIFT => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    self.accum = reg1 << opcode[2];
+                }
+                RSHIFT => {
+                    let reg1 = self.regs[opcode[1] as usize];
+                    self.accum = reg1 >> opcode[2];
+                }
+                PUSH => {
+                    let reg = self.regs[opcode[1] as usize];
+                    self.mem[self.sp] = reg as u8;
+                    self.mem[self.sp - 1] = (reg >> 8) as u8;
+                    self.sp -= 2;
+                }
+                POP => {
+                    self.regs[opcode[1] as usize] =
+                        ((self.mem[self.sp + 2] as u16) << 8) | (self.mem[self.sp + 1] as u16);
+                    self.sp += 2;
+                }
+                DUMP_ACCUM => {
+                    let index = (((opcode[2] as u16) << 8) | (opcode[3] as u16)) as usize;
+                    self.mem[index] = (self.accum >> 8) as u8;
+                    self.mem[index + 1] = self.accum as u8;
+                }
+                PUSH_ACCUM => {
+                    self.mem[self.sp] = self.accum as u8;
+                    self.mem[self.sp - 1] = (self.accum >> 8) as u8;
+                    self.sp -= 2;
+                }
+                HALT => {
                     self.halted = true;
                 }
+                _ => (),
             }
         }
 
@@ -60,14 +144,20 @@ impl VM {
         self.mem[self.instruc] = (opcode >> 8) as u8;
         self.mem[self.instruc + 1] = opcode as u8;
     }
+    fn run(&mut self, opcode: u16) {
+        self.insert_opcode(opcode);
+        self.step();
+    }
 }
 
 impl Default for VM {
     fn default() -> Self {
         Self {
-            mem: Vec::with_capacity(MEMORY),
+            mem: Vec::with_capacity(MEMORY + 1),
             regs: [0; 16],
             instruc: 0,
+            sp: MEMORY,
+            accum: 0,
             halted: false,
         }
     }
